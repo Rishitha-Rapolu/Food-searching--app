94a9ec91175b9caac07279c162a338062d1d6fe1
Rishitha-Rapolu <84693529+Rishitha-Rapolu@users.noreply.github.com>
2022-08-19 01:51:30 +0530

    Add files via upload

---
+++ food -recipe-app/Food-Recipe-react/build/precache-manifest.13c790a50f608c4585a5814fd40a0a95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5346653b9f07d4cf83595b3463f404f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4be0cdb6b441bc953d6",
+    "url": "/static/css/main.176b7c23.chunk.css"
+  },
+  {
+    "revision": "d56b1daee22fc6190aff",
+    "url": "/static/js/2.20cc933b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.20cc933b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4be0cdb6b441bc953d6",
+    "url": "/static/js/main.70aa3936.chunk.js"
+  },
+  {
+    "revision": "559ea9b268b7819359f3",
+    "url": "/static/js/runtime-main.786b8bbc.js"
+  }
+]);
